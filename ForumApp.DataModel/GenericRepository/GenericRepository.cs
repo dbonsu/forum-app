7975956c337d22ac67060ae2c1086ac360ffba2d
derick <dbonsu2012@gmail.com>
2015-12-24 00:30:48 -0500

    added interface for generic repo

---
+++ ForumApp.DataModel/GenericRepository/GenericRepository.cs
@@ -0,0 +1,136 @@
+﻿using ForumApp.DataModel.GenericRepository;
+using ForumApp.DataModel.GenericRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumApp.DataModel.GenericRepository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal ForumEntities Context;
+        internal DbSet<TEntity> DbSet;
+        private bool dispose = false;
+
+        public GenericRepository(ForumEntities context)
+        {
+            this.Context = context;
+            this.DbSet = context.Set<TEntity>();
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = DbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+
+        public void Delete(Func<TEntity, Boolean> where)
+        {
+            IQueryable<TEntity> objects = DbSet.Where<TEntity>(where).AsQueryable();
+            foreach (TEntity obj in objects)
+                DbSet.Remove(obj);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!dispose)
+            {
+                if (disposing)
+                {
+                    this.Context.Dispose();
+                }
+                dispose = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public bool EntityExists(object id)
+        {
+            return DbSet.Find(id) != null;
+        }
+
+        public virtual IEnumerable<TEntity> Get()
+        {
+            IQueryable<TEntity> query = DbSet;
+            return query.ToList();
+        }
+
+        public virtual TEntity Get(Func<TEntity, Boolean> where)
+        {
+            return DbSet.Where(where).FirstOrDefault<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).ToList();
+        }
+
+        public virtual IQueryable<TEntity> GetAllAsQueryable(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).AsQueryable();
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual TEntity GetFirst(Func<TEntity, bool> predicate)
+        {
+            return DbSet.First<TEntity>(predicate);
+        }
+
+        public virtual TEntity GetSingle(Func<TEntity, bool> predicate)
+        {
+            return DbSet.Single<TEntity>(predicate);
+        }
+
+        /// <summary>
+        /// Inclue multiple
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="include"></param>
+        /// <returns></returns>
+        public virtual IQueryable<TEntity> GetWithInclude(
+            System.Linq.Expressions.Expression<Func<TEntity,
+            bool>> predicate, params string[] include)
+        {
+            IQueryable<TEntity> query = this.DbSet;
+            query = include.Aggregate(query, (current, inc) => current.Include(inc));
+            return query.Where(predicate);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            DbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ ForumApp.DataModel/GenericRepository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumApp.DataModel.GenericRepository.Interfaces
+{
+    public interface IGenericRepository<TEntity> : IDisposable
+    {
+        /// <summary>
+        /// Deletes entity in db based on id
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(object id);
+
+        /// <summary>
+        /// Generic Delete method for the entities
+        /// </summary>
+        /// <param name="entityToDelete"></param>
+        void Delete(TEntity entityToDelete);
+
+        /// <summary>
+        /// Overloaded Delete based on condition
+        /// </summary>
+        /// <param name="where"></param>
+        void Delete(Func<TEntity, Boolean> where);
+
+        /// <summary>
+        /// Checks if entity exits
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool EntityExists(object id);
+
+        /// <summary>
+        /// Returns Entity based on boolean
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        TEntity Get(Func<TEntity, Boolean> where);
+
+        /// <summary>
+        /// Get all the records from db
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<TEntity> GetAll();
+
+        /// <summary>
+        /// Overloaded GetAll based on condition
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        IEnumerable<TEntity> GetAll(Func<TEntity, bool> where);
+
+        /// <summary>
+        /// Returns Entities as queryable
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        IQueryable<TEntity> GetAllAsQueryable(Func<TEntity, bool> where);
+
+        /// <summary>
+        ///Get Entity based on id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TEntity GetByID(object id);
+
+        /// <summary>
+        /// Gets first Entity in db
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        TEntity GetFirst(Func<TEntity, bool> predicate);
+
+        /// <summary>
+        /// DbSet implementation of single method which returns the only entity matches the predicate
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        TEntity GetSingle(Func<TEntity, bool> predicate);
+
+        /// <summary>
+        /// Inclue multiple
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="include"></param>
+        /// <returns></returns>
+        IQueryable<TEntity> GetWithInclude(
+             System.Linq.Expressions.Expression<Func<TEntity,
+             bool>> predicate, params string[] include);
+
+        /// <summary>
+        /// Inserts an entity in to db
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(TEntity entity);
+
+        /// <summary>
+        /// Generic update method for the entities
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(TEntity entity);
+    }
+}
