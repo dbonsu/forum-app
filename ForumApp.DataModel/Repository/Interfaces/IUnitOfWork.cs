4b2ee7e6aa92038c223fdd254240a177b37fd722
derick <dbonsu2012@gmail.com>
2015-12-24 12:36:48 -0500

    implemented unitofwork

--- ForumApp.DataModel/Repository/Interfaces/IUnitOfWork.cs
+++ ForumApp.DataModel/Repository/Interfaces/IUnitOfWork.cs
@@ -8,5 +8,21 @@ namespace ForumApp.DataModel.Repository.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
+        GenericRepository<Forum> ForumRepository { get; set; }
+        GenericRepository<ForumThread> ForumThreadRepository { get; set; }
+        GenericRepository<InstantMessageReply> InstantMessageReply { get; set; }
+        GenericRepository<InstantMessage> InstantMessageRepository { get; set; }
+        GenericRepository<LoginAttempt> LoginAttemptRepository { get; set; }
+        GenericRepository<Password> PasswordRepository { get; set; }
+        GenericRepository<Profile> ProfileRepository { get; set; }
+        GenericRepository<Role> RoleRepository { get; set; }
+        GenericRepository<ThreadReply> ThreadReplyRepository { get; set; }
+        GenericRepository<Topic> TopicRepository { get; set; }
+        GenericRepository<User> UserRepository { get; set; }
+
+        /// <summary>
+        /// Save method.
+        /// </summary>
+        void Save();
     }
 }
