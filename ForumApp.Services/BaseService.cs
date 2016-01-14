f2abec82b5fffb7e6f9f25635f9d3b090cb75c77
derick <dbonsu2012@gmail.com>
2016-01-13 19:45:03 -0500

    no repo

---
+++ ForumApp.Services/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using ForumApp.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumApp.Services
+{
+    public class BaseService
+    {
+        private ForumAppEntities _context;
+    }
+}
